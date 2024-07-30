1f972b15017e34f065e3c42c9ce08b34394ec48a
SuperHotDogCat <siromisochan@gmail.com>
2024-07-30 14:57:24 +0900

    step4 エラーメッセージの改善

--- make_calculator/9cc.c
+++ make_calculator/9cc.c
@@ -28,6 +28,23 @@ Token *token;
 
 // エラーを報告するための関数
 // printfと同じ引数を取る
+
+// ひとまず入力プログラムargv[1]を格納する変数
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...){
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input; // locから何バイト離れているかを示すint
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, ""); // %*sでsを指定した文字数だけ表示する
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -50,7 +67,7 @@ bool consume(char op){
 // それ以外の時にはエラーを報告する
 void expect(char op){
   if (token->kind != TK_RESERVED || token->str[0] != op){
-    error("'%c'ではありません。", op);
+    error_at(token->str,"'%c'ではありません。", op);
   }
   token = token->next;
 }
@@ -59,7 +76,7 @@ void expect(char op){
 // それ以外の場合にはエラーを報告する
 int expect_number(){
   if (token->kind != TK_NUM){
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   }
   int val = token->val;
   token = token->next;
@@ -100,7 +117,7 @@ Token *tokenize(char *p){
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(p, "トークナイズできません");
   }
   new_token(TK_EOF, cur, p);
   return head.next;
@@ -111,6 +128,7 @@ int main(int argc, char **argv){
     error("引数の個数が正しくありません");
     return 1;
   }
+  user_input = argv[1]; // 先にuser_inputにargvを渡しておかないとuser_inputがnullであるため注意
   token = tokenize(argv[1]);
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
